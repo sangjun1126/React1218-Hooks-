bfab6582e48d67a0c7368a2db376e0d2c7e8248f
sangjun1126 <jeongsangjun1234@gmail.com>
2023-12-18 15:41:19 +0900

    Add project files

---
+++ src/Average.js
@@ -0,0 +1,44 @@
+import { useState, useMemo, useCallback, useRef } from "react";
+
+const getAverage = (numbers) => {
+  console.log("평균 값 계산중입니다");
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((a, b) => a + b);
+  return sum / numbers.length;
+};
+
+const Average = () => {
+  const [list, setList] = useState([]);
+  const [number, setNumber] = useState("");
+  const inputEl = useRef(null);
+
+  const onChange = useCallback((e) => {
+    setNumber(e.target.value);
+  }, []); // 컴포넌트가 처음 렌더링 될 경우에만 함수를 생성합니다.
+
+  const onInsert = useCallback(() => {
+    const nextList = list.concat(parseInt(number));
+    setList(nextList);
+    setNumber("");
+    inputEl.current.focus();
+  }, [number, list]); // number 혹은 list가 바뀌었을 경우에만 수행한다.
+
+  const avg = useMemo(() => getAverage(list), [list]);
+
+  return (
+    <div>
+      <input value={number} onChange={onChange} ref={inputEl} />
+      <button onClick={onInsert}>등록</button>
+      <ul>
+        {list.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+      <div>
+        <b>평균값 :</b> {avg}
+      </div>
+    </div>
+  );
+};
+
+export default Average;
---
+++ src/Counter.js
@@ -0,0 +1,30 @@
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  // action.type에 따라 다른 작업을 수행함
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 };
+    case "DECREMENT":
+      return { value: state.value - 1 };
+    default:
+      // 아무것도 해당되지 않을 경우 기존 상태를 반환함
+      return state;
+  }
+}
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
+  return (
+    <div>
+      <p>
+        현재 카운터 값은 <b>{state.value}</b>입니다.
+      </p>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+1</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-1</button>
+    </div>
+  );
+};
+
+export default Counter;
